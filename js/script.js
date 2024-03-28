36c9ccf373750561300a0db597213896fdad6e11
GrimdvlDesktop <grimdvl@gmail.com>
2024-03-28 08:59:42 +0300

    create html css

--- js/script.js
+++ js/script.js
@@ -11,21 +11,31 @@ document.addEventListener('DOMContentLoaded', function() {
                 const productCard = document.createElement('div');
                 const productTitle = document.createElement('h2');
                 const productPrice = document.createElement('p');
+                const productPriceNum = document.createElement('span');
+                const productPriceText = document.createElement('p');
                 const productDescription = document.createElement('p');
                 const productImage = document.createElement('img');
                 const productDescrButton = document.createElement('button');
+                const productCategory = document.createElement('button');
+                const productPriceAdd = document.createElement('button');
 
-                const maxLength = 40;
+                const maxLength = 50;
 
                 productCard.classList.add('products__card');
                 productTitle.classList.add('products__card-title');
                 productDescription.classList.add('products__card-description');
                 productImage.classList.add('products__card-img')
                 productDescrButton.classList.add('description--show');
+                productCategory.classList.add('products__card-category');
+                productPrice.classList.add('products__card-price');
+                productPriceAdd.classList.add('price--adding');
+                productPriceNum.classList.add('price-num');
+                productPriceText.classList.add('price-text');
 
                 productTitle.textContent = product.title;
-                productPrice.textContent = `Price: $${product.price}`;
-                productDescrButton.textContent = '...';
+                productPriceText.innerHTML = `Price <br> <br>`;
+                productPriceNum.textContent = `$ ${product.price}`;
+                productDescrButton.textContent = '...Read More';
                 if (product.description.length > maxLength) {
                     productDescription.textContent = `${product.description.slice(0, maxLength)}`;
                     productDescrButton.addEventListener('click', () => {
@@ -36,20 +46,25 @@ document.addEventListener('DOMContentLoaded', function() {
                     productDescription.textContent = product.description;
                     productDescrButton.style.display = 'none';
                 }
-
                 productImage.onerror = () => {
                     productImage.src = placeholderImage;
                     productImage.alt = 'Placeholder Image';
                 };
                 productImage.src = product.images[0];
                 productImage.alt = product.title;
+                productCategory.textContent = product.category.name;
+                productPriceAdd.textContent = 'Add to Cart'
 
                 productCard.appendChild(productImage);
                 productCard.appendChild(productTitle);
-                productCard.appendChild(productPrice);
                 productCard.appendChild(productDescription);
+                productCard.appendChild(productCategory);
                 productDescription.appendChild(productDescrButton);
                 productsContainer.appendChild(productCard);
+                productCard.appendChild(productPrice);
+                productPrice.appendChild(productPriceText);
+                productPriceText.appendChild(productPriceNum);
+                productPrice.appendChild(productPriceAdd);
             });
         })
         .catch(error => {
